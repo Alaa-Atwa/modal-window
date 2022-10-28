f1b19542fb86234ce35a39258d5021d3870583a8
Alaa Atwa <alaa.atwa555@gmail.com>
2022-10-28 12:33:52 +0200

    adding and removing classes

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+
+const btnOpenModal = document.querySelectorAll('.show-modal'); // to select multiple have the same class in this case
+
+console.log(btnOpenModal);
+
+// for (let i = 0; i < btnOpenModal.length; i++)
+//   console.log(btnOpenModal[i].textContent);
+// adding event handler
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden'); // notice we don't use the dot in class name
+    // modal.classList.add('hidden')
+  });
+
