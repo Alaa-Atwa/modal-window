513968adfcbfed96bfb6c4e1e0ab10be0c242cc0
Alaa Atwa <alaa.atwa555@gmail.com>
2022-10-28 13:06:21 +0200

    dry code

--- script.js
+++ script.js
@@ -5,20 +5,21 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModel = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal'); // to select multiple have the same class in this case
 
-console.log(btnOpenModal);
-
-// for (let i = 0; i < btnOpenModal.length; i++)
-//   console.log(btnOpenModal[i].textContent);
-// adding event handler
-
-for (let i = 0; i < btnOpenModal.length; i++)
-  btnOpenModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-
-btnCloseModel.addEventListener('click', function () {
+// functions
+const closeModel = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// event listeners
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModel.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModel);
 
