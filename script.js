6c14aaf257347801c2a07eef3e543804677a50e2
Alaa Atwa <alaa.atwa555@gmail.com>
2022-10-28 13:40:27 +0200

    closing the modal when pressing ESC

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const btnCloseModel = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal'); // to select multiple have the same class in this case
 
 // functions
-const closeModel = function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
@@ -21,5 +21,19 @@ for (let i = 0; i < btnOpenModal.length; i++)
   btnOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModel.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModal);
+
+// global events happens over the document like button clicks
+// closing the modal when the ESC pressed
+
+document.addEventListener('keydown', function (event) {
+  // you can put "e" or anything instead of "event"
+  // the event argument passed as an argument
+  if (event.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      // if the model doesn't contain the 'hidden' class
+      closeModal();
+    }
+  }
+});
 
